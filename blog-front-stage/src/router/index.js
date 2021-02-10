36ba529b2eb949e4c0a5cddad6f213d5e27d7dec
syy11955 <klaysun11@gmail.com>
2021-02-11 00:33:38 +0800

    header use

--- blog-front-stage/src/router/index.js
+++ blog-front-stage/src/router/index.js
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import Archive from '../components/archive';
+import Header from '../components/universal/header';
 
 function Index() {
   return <h1>Index</h1>;
@@ -11,11 +12,14 @@ function List() {
 
 function AppRouter() {
   return (
-    <Router>
-      <Route path="/" exact component={Index} />
-      <Route path="/list/" component={List} />
-      <Route path="/archive" component={Archive} />
-    </Router>
+    <div>
+      <Router>
+        <Header />
+        <Route path="/" exact component={Index} />
+        <Route path="/list/" component={List} />
+        <Route path="/archive" component={Archive} />
+      </Router>
+    </div>
   );
 }
 
