ed35fa04158ceaacbaffd44f5d9d1567c2ec299b
shaunyoung11 <shaunyoung11@163.com>
2021-02-14 15:17:24 +0800

    separeate header API from index

--- blog-front-stage/src/store/actionCreators.js
+++ blog-front-stage/src/store/actionCreators.js
@@ -1,23 +1,39 @@
 import axios from 'axios';
-import { GET_ARTICLE } from './actionTypes';
+import { GET_ARTICLE, GET_HEADER } from './actionTypes';
 
+const root =
+  'https://www.fastmock.site/mock/6ed5b8c00a26ca0e931dcba01b79b475/myblog';
+
+/**
+ * 获取文章信息
+ * @param {String} type
+ */
 export const getArticle = (type) => {
   return (dispatch) => {
     console.log(type);
-    axios
-      .get(
-        'https://www.fastmock.site/mock/6ed5b8c00a26ca0e931dcba01b79b475/myblog/front/getArticles/' +
-          type
-      )
-      .then((res) => {
-        console.log(res);
-        const action = getArticleAction(res);
-        dispatch(action);
-      });
+    axios.get(root + '/front/getArticles/' + type).then((res) => {
+      console.log(res);
+      const action = getArticleAction(res.data.data);
+      dispatch(action);
+    });
   };
 };
 
 const getArticleAction = (value) => ({
   type: GET_ARTICLE,
+  value,
+});
+
+export const getHeader = () => {
+  return (dispatch) => {
+    axios.get(root + '/front/getHeader').then((res) => {
+      const action = getHeaderAction(res.data.data);
+      dispatch(action);
+    });
+  };
+};
+
+const getHeaderAction = (value) => ({
+  type: GET_HEADER,
   value,
 });
--- blog-front-stage/src/store/reducer.js
+++ blog-front-stage/src/store/reducer.js
@@ -1,13 +1,24 @@
-import { GET_ARTICLE } from './actionTypes';
+import { GET_ARTICLE, GET_HEADER } from './actionTypes';
 
-const defaultState = {};
+const defaultState = {
+  showNav: false,
+  siteInfo: {},
+};
 // eslint-disable-next-line
 export default (state = defaultState, action) => {
   if (action.type === GET_ARTICLE) {
-    console.log(action.value.data.data);
+    console.log(action);
     // eslint-disable-next-line
     let newState = JSON.parse(JSON.stringify(state));
     // 对 newState 中的数据进行更新
+    newState.articles = action.value.articles;
+    return newState;
+  }
+  if (action.type === GET_HEADER) {
+    console.log(action.value);
+    let newState = JSON.parse(JSON.stringify(state));
+    newState.siteInfo = action.value.siteInfo;
+    return newState;
   }
   return state;
 };
