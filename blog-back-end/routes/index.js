75ab2701a4dd069e680623bb76ed38a4c5292db2
shaunyoung11 <shaunyoung11@163.com>
2021-03-02 22:04:53 +0800

    add route for articles query

--- blog-back-end/routes/index.js
+++ blog-back-end/routes/index.js
@@ -1,9 +1,42 @@
 var express = require('express');
 var router = express.Router();
+var model = require('../model');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+/**
+ * 获取主页或归档页文章内容
+ * :group - 文章分组信息，若查询全部则为 all
+ * :current - 当前页码
+ * :pagesize - 每页的容量，用于分页查询
+ */
+router.get(
+  '/front/getArticles/:group/:current/:pagesize',
+  function (req, res, next) {
+    let group = req.params.group;
+    let current = req.params.current;
+    let pagesize = req.params.pagesize;
+    model.connect((db) => {
+      db.collection('articles')
+        .find({ group: group })
+        .skip((current - 1) * pagesize)
+        .limit(parseInt(pagesize))
+        .toArray((err, docs) => {
+          if (err) {
+            res.send({
+              code: 400,
+              msg: 'Find data error!',
+            });
+          } else {
+            res.send({
+              code: 200,
+              msg: 'Succeed!',
+              data: {
+                articles: docs,
+              },
+            });
+          }
+        });
+    });
+  }
+);
 
 module.exports = router;
