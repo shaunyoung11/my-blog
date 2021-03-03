1091da1f53593609359f30d625995bdf4dee7bf0
shaunyoung11 <shaunyoung11@163.com>
2021-03-03 14:57:49 +0800

    add Link to posts

--- blog-front-stage/src/router/index.js
+++ blog-front-stage/src/router/index.js
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Archive from '../components/archive';
 import About from '../components/pages/about';
 import Links from '../components/pages/links';
+import Post from '../components/post';
 import Footer from '../components/universal/footer';
 import Header from '../components/universal/header';
 
@@ -17,6 +18,7 @@ function AppRouter() {
         <Route path="/links" component={Links} />
         <Route path="/about" component={About} />
         <Route path="/archives/:group" component={Archive} />
+        <Route path="/post/:cid" component={Post} />
         <Footer></Footer>
       </Router>
     </div>
