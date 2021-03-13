10873d9866c88ca40a9283c888171379504811aa
shaunyoung11 <shaunyoung11@163.com>
2021-03-13 22:07:04 +0800

    add js to link page

--- blog-front-stage/src/components/pages/links.js
+++ blog-front-stage/src/components/pages/links.js
@@ -1,9 +1,42 @@
 import React, { Component } from 'react';
+import store from '../../store';
+import { getLink } from '../../store/actionCreators';
 
 class Links extends Component {
-  state = {};
+  constructor(props) {
+    super(props);
+    this.state = store.getState();
+    this.storeChange = this.storeChange.bind(this);
+    store.subscribe(this.storeChange);
+  }
   render() {
-    return <div className="content-main">Links</div>;
+    return (
+      <div className="content-main">
+        <div className="link-list">
+          {this.state.link.map((item, index) => {
+            return (
+              <a
+                href={item.url}
+                className="list-item"
+                title={item.title}
+                key={item + index}
+              >
+                <img className="item-logo" src={item.logo} alt={item.title} />
+                <p className="item-name">{item.name}</p>
+              </a>
+            );
+          })}
+        </div>
+        <div className="statement"></div>
+      </div>
+    );
+  }
+  componentDidMount() {
+    const action = getLink();
+    store.dispatch(action);
+  }
+  storeChange() {
+    this.setState(store.getState());
   }
 }
 
