9571fa9b388446074021438e0761a731c8980d95
syy11955 <klaysun11@gmail.com>
2021-03-13 23:40:12 +0800

    finish about page

--- blog-front-stage/src/components/pages/about.js
+++ blog-front-stage/src/components/pages/about.js
@@ -1,9 +1,28 @@
 import React, { Component } from 'react';
+import store from '../../store';
+import { getAbout } from '../../store/actionCreators';
 
 class About extends Component {
-  state = {};
+  constructor(props) {
+    super(props);
+    this.state = store.getState();
+    this.storeChange = this.storeChange.bind(this);
+    store.subscribe(this.storeChange);
+  }
   render() {
-    return <div className="content-main">About</div>;
+    return (
+      <div
+        className="content-main"
+        dangerouslySetInnerHTML={{ __html: this.state.about.content }}
+      ></div>
+    );
+  }
+  componentDidMount() {
+    const action = getAbout();
+    store.dispatch(action);
+  }
+  storeChange() {
+    this.setState(store.getState());
   }
 }
 
--- blog-front-stage/src/store/actionTypes.js
+++ blog-front-stage/src/store/actionTypes.js
@@ -2,3 +2,4 @@ export const GET_ARTICLE = 'getArticle';
 export const GET_HEADER = 'getHeader';
 export const GET_POST = 'getPost';
 export const GET_LINK = 'getLink';
+export const GET_ABOUT = 'getAbout';
