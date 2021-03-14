789f18757590e71fa7acebb9a58c5dc78f357108
syy11955 <klaysun11@gmail.com>
2021-03-14 20:06:36 +0800

    add element and init basic layout

---
+++ blog-back-stage/src/plugin/element.js
@@ -0,0 +1,8 @@
+import Vue from 'vue';
+import { Container, Header, Aside, Main, Footer } from 'element-ui';
+
+Vue.use(Container);
+Vue.use(Header);
+Vue.use(Aside);
+Vue.use(Main);
+Vue.use(Footer);
