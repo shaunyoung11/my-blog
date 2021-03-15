ab3864363e17a7296dfebbbaabdb5201f428cc2f
shaunyoung11 <shaunyoung11@163.com>
2021-03-15 22:57:25 +0800

    finish overview page

--- blog-back-stage/src/components/blog/Overview.vue
+++ blog-back-stage/src/components/blog/Overview.vue
@@ -1,47 +1,22 @@
 <template>
   <div class="overview">
     <el-form>
+      <el-avatar shape="circle" :src="logoShow"></el-avatar>
       <!-- 站点logo -->
-      <el-form-item label="站点 Logo">
-        <el-avatar shape="circle" :src="logo"></el-avatar>
-        <el-input placeholder="请输入站点logo图片地址" v-model="logoInput">
-          <el-button slot="append" @click="handleChangeLogo">修改</el-button>
-        </el-input>
-      </el-form-item>
-      <!-- 站点名称 -->
-      <el-form-item label="站点名称">
-        <el-input placeholder="请输入站点名称" v-model="name">
-          <el-button slot="append" @click="handleChangeName">修改</el-button>
-        </el-input>
-      </el-form-item>
-      <!-- 首页地址 -->
-      <el-form-item label="首页地址">
-        <el-input placeholder="请输入站点首页地址" v-model="url">
-          <el-button slot="append" @click="handleChangeUrl">修改</el-button>
-        </el-input>
-      </el-form-item>
-      <!-- 站点简介 -->
-      <el-form-item label="站点简介">
-        <el-input placeholder="请输入站点简介" v-model="intro">
-          <el-button slot="append" @click="handleChangeIntro">修改</el-button>
-        </el-input>
-      </el-form-item>
-      <!-- 站点备案信息 -->
-      <el-form-item label="站点备案信息">
-        <el-input placeholder="请输入 icp 备案号" v-model="icp">
-          <el-button slot="append">修改</el-button>
-        </el-input>
-      </el-form-item>
-      <!-- github地址 -->
-      <el-form-item label="GitHub地址">
-        <el-input placeholder="请输入 GitHub 地址" v-model="github">
-          <el-button slot="append">修改</el-button>
-        </el-input>
-      </el-form-item>
-      <!-- 邮箱 -->
-      <el-form-item label="邮箱地址">
-        <el-input placeholder="请输入邮箱地址" v-model="mail">
-          <el-button slot="append">修改</el-button>
+      <el-form-item
+        v-for="item in siteInfo"
+        :key="item.name"
+        :label="item.label"
+      >
+        <el-input
+          autocomplete="on"
+          :name="item.name"
+          :placeholder="item.placeholder"
+          v-model="item.value"
+        >
+          <el-button slot="append" @click="handleChangeInfo">{{
+            item.btn
+          }}</el-button>
         </el-input>
       </el-form-item>
     </el-form>
@@ -52,47 +27,37 @@
 export default {
   data() {
     return {
-      logoInput: "",
-      logo: "",
-      name: "",
-      url: "",
-      intro: "",
-      icp: "",
-      github: "",
-      mail: "",
+      logoShow: "",
+      siteInfo: [],
     };
   },
 
   methods: {
-    // 处理更换站点logo事件
-    handleChangeLogo() {
-      console.log(this.$data.logoInput);
-      this.$data.logo = this.$data.logoInput;
-    },
-    // 处理更换站点名称事件
-    handleChangeName() {
-      console.log(this.$data.name);
-    },
-    // 处理更改站点首页地址事件
-    handleChangeUrl() {},
-    // 处理更改站点简介事件
-    handleChangeIntro() {
-      console.log(this.$data.intro);
+    // 处理更改网站信息事件
+    handleChangeInfo() {
+      console.log(this.siteInfo);
+      this.$data.logoShow = this.siteInfo[0].value;
+      this.$http
+        .post("/back/postInfo", { siteInfo: this.siteInfo })
+        .then((res) => {
+          console.log(res);
+          if (res.data.msg === "Succeed!") {
+            this.$notify({
+              title: "成功",
+              message: "修改站点信息成功",
+              type: "success",
+              duration: 1000,
+            });
+          }
+        });
     },
   },
 
   mounted() {
     this.$http.get("/front/getHeader").then((res) => {
       console.log(res);
-      let siteInfo = res.data.data.siteInfo[0];
-      this.$data.logo = siteInfo.logo;
-      this.$data.logoInput = siteInfo.logo;
-      this.$data.name = siteInfo.name;
-      this.$data.url = siteInfo.url;
-      this.$data.intro = siteInfo.intro;
-      this.$data.icp = siteInfo.icp;
-      this.$data.github = siteInfo.github;
-      this.$data.mail = siteInfo.mail;
+      this.$data.siteInfo = res.data.data[0].siteInfo;
+      this.$data.logoShow = this.siteInfo[0].value;
     });
   },
 };
