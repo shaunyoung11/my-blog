7b6cf716084a433ccaa12afa8592e2df67cb727c
shaunyoung11 <shaunyoung11@163.com>
2021-03-15 11:20:06 +0800

    add sidebar menu

--- blog-back-stage/src/router/index.js
+++ blog-back-stage/src/router/index.js
@@ -1,14 +1,28 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
 import Home from '../views/Home';
+import Write from '../components/article/Write';
+import Manage from '../components/article/Manage';
 
 Vue.use(VueRouter);
 
 const routes = [
   {
-    path: '/',
+    path: '/home',
     name: 'Home',
     component: Home,
+    children: [
+      {
+        path: '/write',
+        name: 'Write',
+        component: Write,
+      },
+      {
+        path: '/manage',
+        name: 'Manage',
+        component: Manage,
+      },
+    ],
   },
 ];
 
