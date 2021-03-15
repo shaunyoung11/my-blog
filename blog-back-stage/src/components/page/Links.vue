8d177a252fdd0d525314eaa6b9b1d01a944513e9
shaunyoung11 <shaunyoung11@163.com>
2021-03-16 00:05:18 +0800

    start to develop page Links

--- blog-back-stage/src/components/page/Links.vue
+++ blog-back-stage/src/components/page/Links.vue
@@ -1,9 +1,44 @@
 <template>
-  <div>this is Links page</div>
+  <div class="links">
+    <el-table fit :data="link" stripe>
+      <el-table-column prop="name" label="友链名称"></el-table-column>
+      <el-table-column prop="url" label="友链地址"></el-table-column>
+      <el-table-column prop="title" label="友链简介">
+        <template slot-scope="scope">
+          <el-input v-model="scope.row.title">
+            <el-button slot="append">修改</el-button>
+          </el-input>
+        </template>
+      </el-table-column>
+      <el-table-column prop="logo" label="友链头像">
+        <template slot-scope="scope">
+          <el-input v-model="scope.row.logo">
+            <el-avatar
+              slot="append"
+              :src="scope.row.logo"
+              shape="square"
+            ></el-avatar>
+          </el-input>
+        </template>
+      </el-table-column>
+    </el-table>
+  </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      link: [],
+    };
+  },
+  mounted() {
+    this.$http.get("/front/getLink").then((res) => {
+      console.log(res);
+      this.$data.link = res.data.data.link;
+    });
+  },
+};
 </script>
 
 <style lang="scss" scoped>
