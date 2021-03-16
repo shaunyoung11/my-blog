92a5157d6ae42f5db33adf2df843101f40139609
syy11955 <klaysun11@gmail.com>
2021-03-16 15:05:38 +0800

    finish view of login

--- blog-back-stage/src/views/Login.vue
+++ blog-back-stage/src/views/Login.vue
@@ -1,10 +1,69 @@
 <template>
-  <div class="login">这里是登录界面</div>
+  <div class="login">
+    <h1>登录到博客后台</h1>
+    <div class="box">
+      <el-avatar :size="80">Blog</el-avatar>
+      <el-input
+        label="用户名"
+        type="text"
+        placeholder="请输入用户名"
+        v-model="username"
+        clearable
+      ></el-input>
+      <el-input
+        label="密码"
+        type="password"
+        placeholder="请输入密码"
+        v-model="password"
+        clearable
+      ></el-input>
+      <el-button @click="handleClickLogin">登录</el-button>
+    </div>
+  </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      username: "",
+      password: "",
+    };
+  },
+  methods: {
+    handleClickLogin() {
+      console.log("login clicked");
+    },
+  },
+};
 </script>
 
 <style lang="scss" scoped>
+.login {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 100%;
+  h1 {
+    margin-top: -100px;
+  }
+  .box {
+    width: 30%;
+    height: 40%;
+    background-color: #fff;
+    border-radius: 25px;
+    padding: 0 5%;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+    align-items: center;
+    .el-avatar--80 {
+      width: 80px;
+      height: 80px;
+      transform: translateY(-50%);
+    }
+  }
+}
 </style>
