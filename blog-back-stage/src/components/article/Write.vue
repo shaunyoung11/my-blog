337616929285c8381db6f02430364305e17f8cde
syy11955 <klaysun11@gmail.com>
2021-03-18 00:25:27 +0800

    add write article page

--- blog-back-stage/src/components/article/Write.vue
+++ blog-back-stage/src/components/article/Write.vue
@@ -1,9 +1,40 @@
 <template>
-  <div>write article here</div>
+  <div class="write">
+    <el-input
+      type="text"
+      placeholder="请输入文章标题"
+      v-model="article.title"
+    ></el-input>
+    <el-input
+      type="textarea"
+      v-model="article.content"
+      :autosize="{ minRows: 20 }"
+    ></el-input>
+    <el-button @click="handlePostArticle">确认</el-button>
+  </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      article: {
+        title: "",
+        content: "",
+      },
+    };
+  },
+  methods: {
+    handlePostArticle() {},
+  },
+  mounted() {
+    if (this.$props.cid) {
+      this.$http.get("/front/getPost").then((res) => {
+        console.log(res);
+      });
+    }
+  },
+};
 </script>
 
 <style lang="scss" scoped>
