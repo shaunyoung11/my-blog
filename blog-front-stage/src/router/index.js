9dd01659be451e6facf1ffe6c043241e1838fa87
shaunyoung11 <shaunyoung11@163.com>
2021-04-03 21:23:41 +0800

    improve router

--- blog-front-stage/src/router/index.js
+++ blog-front-stage/src/router/index.js
@@ -3,25 +3,34 @@ import Archive from '../pages/index';
 import About from '../pages/about/about';
 import Links from '../pages/link/link';
 import Post from '../pages/post/post';
-import Footer from '../components/universal/footer';
-import Header from '../components/universal/header';
+import MyFooter from '../components/universal/footer';
+import MyHeader from '../components/universal/header';
+import { Layout } from 'antd';
+
+const { Header, Content, Footer } = Layout;
 
 function AppRouter() {
   return (
-    <div>
-      <Router>
-        <Header
-          logoUrl="https://shaun-logo.oss-cn-beijing.aliyuncs.com/my_info.png"
-          siteName="小扬Shaun"
-        />
-        <Route path="/" exact component={Archive} />
-        <Route path="/links" component={Links} />
-        <Route path="/about" component={About} />
-        <Route path="/archives/:group" component={Archive} />
-        <Route path="/post/:cid" component={Post} />
-        <Footer></Footer>
-      </Router>
-    </div>
+    <Router>
+      <Layout>
+        <Header>
+          <MyHeader
+            logoUrl="https://shaun-logo.oss-cn-beijing.aliyuncs.com/my_info.png"
+            siteName="小扬Shaun"
+          />
+        </Header>
+        <Content>
+          <Route path="/" exact component={Archive} />
+          <Route path="/links" component={Links} />
+          <Route path="/about" component={About} />
+          <Route path="/archives/:group" component={Archive} />
+          <Route path="/post/:cid" component={Post} />
+        </Content>
+        <Footer>
+          <MyFooter></MyFooter>
+        </Footer>
+      </Layout>
+    </Router>
   );
 }
 
