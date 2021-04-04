ad6c17e1152df6ed2cd59740f890fa36e373e996
syy11955 <klaysun11@gmail.com>
2021-04-04 20:45:27 +0800

    improve about page

--- blog-back-stage/src/components/page/About.vue
+++ blog-back-stage/src/components/page/About.vue
@@ -1,11 +1,12 @@
 <template>
   <div class="about">
     <el-input
+      class="input"
       type="textarea"
       v-model="about.content"
       :autosize="{ minRows: 20 }"
     ></el-input>
-    <el-button @click="handleSubmitAbout">确认</el-button>
+    <el-button class="btn" @click="handleSubmitAbout">确认</el-button>
   </div>
 </template>
 
@@ -25,6 +26,21 @@ export default {
         })
         .then((res) => {
           console.log(res);
+          if (res.data.msg === "Succeed!") {
+            this.$notify({
+              title: "成功",
+              message: "修改成功！",
+              duration: 1000,
+              type: "success",
+            });
+          } else {
+            this.$notify({
+              title: "失败",
+              message: "修改失败，请检查服务器设置！",
+              duration: 1000,
+              type: "info",
+            });
+          }
         });
     },
   },
@@ -37,4 +53,19 @@ export default {
 };
 </script>
 <style lang="scss" scoped>
+.about {
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  .btn {
+    margin-top: 20px;
+  }
+}
+@media (max-width: 800px) {
+  .about {
+    .btn {
+      width: 100%;
+    }
+  }
+}
 </style>
